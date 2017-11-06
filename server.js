62b9572510627e31241780827adf68fe66f431e0
Matthew Schupack <schu34@gmail.com>
2017-11-06 09:17:03 -0500

    set up service files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const authRouter = require("./services/auth")
+const productRouter = require("./services/product");
+const userRouter = require("./services/user");
+const imageRouter = require("./services/image");
+const paymentRouter = require("./services/payments");
+
+const app = express();
+
+
+
+app.use(express.json())
+
+app.use("/auth", authRouter);
+app.use("/product", productRouter);
+app.use("/user", userRouter);
+app.use("/image", imageRouter);
+app.use("/payments", paymentRouter);
+
+
---
+++ services/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router;
+
+
+
+
+//implement routes here
+
+
+
+
+
+
+module.exports = router;
