daa9548cc11e15d5770ff74a05e1c518e60558aa
Matthew Schupack <schu34@gmail.com>
2017-11-10 11:12:24 -0500

    skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,19 @@ import { LinkContainer }                            from 'react-router-bootstrap
 import { BrowserRouter as Router, Route, Switch, Link }     from "react-router-dom";
 import { connect }                                  from "react-redux";
 import { bindActionCreators }                       from "redux";
-import { editPersona}                               from './actions/action_index';
-import { createNewFlow }                            from "./actions/action_flow";
+
+//components
+import Login from "./Login";
+import Signup from "./Signup";
+import ForgotPassword from "./ForgotPassword";
+import Dashboard from "./Dashboard";
+import Store from "./Store";
+import Product from "./Product";
+
+//css
 import                                               "bootstrap/dist/css/bootstrap.css";
-import '../css/App.css';
+import                                               '../css/App.css';
+
 
 class App extends Component {
   render() {
@@ -18,7 +27,8 @@ class App extends Component {
           <Route path="/signup" component={Signup} />
           <Route path="/forgotPassword" component={ForgotPassword} />
           <Route path="/dashboard" component={Dashboard} />
-          <Route path="/:storeId" component={Store} />
+          <Route path="/store/:storeId" component={Store} />
+          <Route path="/products/:productId" component={Product} />
         </Switch>
       </Router>
     );
