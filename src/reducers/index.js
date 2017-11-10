a8c12bc7bb438fa3d491002e4644494c2166826c
Matthew Schupack <schu34@gmail.com>
2017-11-10 09:51:43 -0500

    kill some comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,7 @@ import { combineReducers } 	from 'redux';
 
 
 const rootReducer = combineReducers({
-  // flow: Flow,
-  // trainer: Trainer,
-  // chatBox: Chatbox,
-  // rasa: Rasa,
-  // authentication: Authentication,
-  // form: Form
+  
 });
 
 export default rootReducer;
