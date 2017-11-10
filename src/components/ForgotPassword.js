d0e6a38b6e85f125d1a8d596781f5e6ce281c2b1
Matthew Schupack <schu34@gmail.com>
2017-11-10 11:34:51 -0500

    skeletons of all the pages

---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col, Glyphicon, Nav, NavItem, Navbar }  from "react-bootstrap";
+import { LinkContainer }                            from 'react-router-bootstrap';
+import { BrowserRouter as Router, Route, Switch, Link }     from "react-router-dom";
+import { connect }                                  from "react-redux";
+import { bindActionCreators }                       from "redux";
+
+//components
+
+//css
+import                                               "bootstrap/dist/css/bootstrap.css";
+import                                               '../css/App.css';
+
+
+class ForgotPassword extends Component {
+  render() {
+    return (
+     <p> this is the ForgotPassword</p>
+    );
+  }
+}
+
+export default ForgotPassword;
