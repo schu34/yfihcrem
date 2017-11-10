517af152e4f7fb404f364d1f8fbf42bf14fac8c6
Matthew Schupack <schu34@gmail.com>
2017-11-10 10:55:17 -0500

    set up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Glyphicon, Nav, NavItem, Navbar }  from "react-bootstrap";
+import { LinkContainer }                            from 'react-router-bootstrap';
+import { BrowserRouter as Router, Route, Switch, Link }     from "react-router-dom";
+import { connect }                                  from "react-redux";
+import { bindActionCreators }                       from "redux";
+import { editPersona}                               from './actions/action_index';
+import { createNewFlow }                            from "./actions/action_flow";
+import                                               "bootstrap/dist/css/bootstrap.css";
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/forgotPassword" component={ForgotPassword} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/:storeId" component={Store} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/store.js
+++ src/store.js
@@ -3,14 +3,8 @@ import { createStore, applyMiddleware } from 'redux';
 import axiosMiddleware 					from 'redux-axios-middleware'
 import reducers 						from './reducers/index.js';
 
-const token = "85C4677E4CDA1B95917E7323F6596"
 const client = axios.create({
 	responseType: 'json',
-	timeout: 20000,
-	params:{
-      	"token": token,
-    },	
-	
 })
 
 const createStoreWithMiddleware = applyMiddleware(axiosMiddleware(client))(createStore);
