f887898465222b6d1a950d036d8feec11d00a876
Matthew Schupack <schu34@gmail.com>
2017-11-10 09:50:58 -0500

    got the boilerplate running

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } 	from 'redux';
+
+
+
+const rootReducer = combineReducers({
+  // flow: Flow,
+  // trainer: Trainer,
+  // chatBox: Chatbox,
+  // rasa: Rasa,
+  // authentication: Authentication,
+  // form: Form
+});
+
+export default rootReducer;
