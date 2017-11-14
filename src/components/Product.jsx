8ae17ba62b33809535b07da948d27ee3ece44282
Excel1559 <Reva8390@gmail.com>
2017-11-14 00:41:17 -0500

    make sure to change table

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,11 +6,23 @@ import { connect }                                  from "react-redux";
 import { bindActionCreators }                       from "redux";
 import Template from './Template';
 
-//components
-
 //css
 import                                               "bootstrap/dist/css/bootstrap.css";
 import                                               '../css/App.css';
+
+var ReactBsTable  = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+
+var products = [{
+      id: 1,
+      name: "Product1",
+      price: 120
+  }, {
+      id: 2,
+      name: "Product2",
+      price: 80
+  }];
 
 
 class Product extends Component {
@@ -19,9 +31,11 @@ class Product extends Component {
     <div>
       <Template>
 
-        <div>
-          <h1>This is a product</h1>
-        </div>
+      <BootstrapTable data={products} striped hover insertRow>
+        <TableHeaderColumn isKey dataField='id'></TableHeaderColumn>
+        <TableHeaderColumn dataField='name'></TableHeaderColumn>
+        <TableHeaderColumn dataField='price'></TableHeaderColumn>
+      </BootstrapTable>
 
       </Template>
     </div>
