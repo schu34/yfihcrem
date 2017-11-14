1463d16aa4cbf68bf618bb09f7223d00135d910c
Matthew Schupack <schu34@gmail.com>
2017-11-14 09:04:22 -0500

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
-import { Grid, Row, Col, Glyphicon, Nav, NavItem, Navbar }  from "react-bootstrap";
-import { LinkContainer }                            from 'react-router-bootstrap';
-import { BrowserRouter as Router, Route, Switch, Link }     from "react-router-dom";
-import { connect }                                  from "react-redux";
-import { bindActionCreators }                       from "redux";
+import React, { Component } from "react";
+import {
+  Grid,
+  Row,
+  Col,
+  Glyphicon,
+  Nav,
+  NavItem,
+  Navbar,
+  Button
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 //components
 
 //css
-import                                               "bootstrap/dist/css/bootstrap.css";
-import                                               '../css/App.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "../css/App.css";
 
 class Login extends Component {
   render() {
     return (
-     <Row>
-       <Col mdOffset={4} md={4}></Col>
-     </Row>
+      <Row>
+        <Col classname="login-container" mdOffset={4} md={4}>
+          <div>
+            <input classname="login-input" type="text" placeholder="email" />
+          </div>
+          <div>
+            <input
+              classname="login-input"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <Button classname="button">Login</Button>
+          <Row>
+            <Col md={6}>google sign .   in</Col> {/*TODO: needs icons*/}
+            <Col md={6}>facebook signin</Col>
+          </Row>
+        </Col>
+      </Row>
     );
   }
 }
