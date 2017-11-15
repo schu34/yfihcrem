e7de38d1be4b0cb282a9bff3c7b7b095ed31ecdd
Matthew Schupack <schu34@gmail.com>
2017-11-15 06:46:03 -0500

    basic google auth routes

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,21 +1,26 @@
 const express = require("express");
-
+const passport = require("passport");
 const router = express.Router();
 
-
-
-
 //implement routes here
-router.post("/login", (req, res)=>{
+router.post("/login", (req, res) => {
   console.log(req.body);
   res.sendStatus(200);
-})
+});
 
-router.post("/signup", (req,res)=>{
-})
+router.post("/signup", (req, res) => {});
 
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-
-
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login"
+  })
+);
 
 module.exports = router;
